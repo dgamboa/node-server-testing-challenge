0d04ef2fe7f362b2affb96e7c5fc556e54a2dfd0
Daniel Gamboa <danielgamboa10@gmail.com>
2021-03-25 17:00:41 -0500

    Add basic structure with all functions and endpoints for controller and model

--- api/companies/companies-model.js
+++ api/companies/companies-model.js
@@ -1,9 +1,25 @@
-const db = require('../../data/db-config');
+const db = require("../../data/db-config");
 
 module.exports = {
   getAll,
-}
+};
 
 function getAll() {
-  return null
+  return null;
+}
+
+function getById(id) {
+  return null;
+}
+
+function create(object) {
+  return null;
+}
+
+function update(id, changes) {
+  return null;
+}
+
+function remove(id) {
+  return null;
 }
--- api/companies/companies-router.js
+++ api/companies/companies-router.js
@@ -8,7 +8,29 @@ router.get("/", (req, res, next) => {
   } catch(err) { next(err) }
 });
 
-// next endpoint
+router.get("/:id", (req, res, next) => {
+  try {
+    return res.json({message: "GET:id endpoint is up"})
+  } catch(err) { next(err) }
+});
+
+router.post("/", (req, res, next) => {
+  try {
+    return res.json({message: "POST endpoint is up"})
+  } catch(err) { next(err) }
+});
+
+router.put("/:id", (req, res, next) => {
+  try {
+    return res.json({message: "PUT endpoint is up"})
+  } catch(err) { next(err) }
+});
+
+router.delete("/:id", (req, res, next) => {
+  try {
+    return res.json({message: "DELETE endpoint is up"})
+  } catch(err) { next(err) }
+});
 
 router.use((err, req, res, next) => { // eslint-disable-line
   res.status(500).json({
